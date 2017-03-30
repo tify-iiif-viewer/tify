3b6ae92f4257ac359f1ef8715206373dcebcedc0
tschaef <tschaef1@gwdg.de>
2017-03-30 19:03:30 +0200

    Update key binding help
    Separate between global and scan-only key bindings. Update heading styles.

--- src/panels/Help.vue
+++ src/panels/Help.vue
@@ -5,6 +5,7 @@
 		<!--TODO-->
 
 		<h3>Key Bindings</h3>
+		<h4>Global</h4>
 		<table>
 			<tr>
 				<th><code>,</code> (comma)</th>
@@ -25,9 +26,22 @@
 				<th><code>End</code></th>
 				<td>Last page</td>
 			</tr>
-
+		</table>
+		<h4>Scan</h4>
+		<table>
 			<tr>
-				<!--TODO: This only works when scan has focus-->
+				<th><code>+</code> (plus)</th>
+				<td>Zoom in</td>
+			</tr>
+			<tr>
+				<th><code>-</code> (minus)</th>
+				<td>Zoom out</td>
+			</tr>
+			<tr>
+				<th><code>&larr;</code> <code>&rarr;</code> <code>&uarr;</code> <code>&darr;</code></th>
+				<td>Pan</td>
+			</tr>
+			<tr>
 				<th><code>0</code> (zero)</th>
 				<td>Reset view</td>
 			</tr>
