2effd3bf2b939ad32cbbf512c08da9357df18a98
tschaef <tschaef1@gwdg.de>
2017-06-02 16:43:52 +0200

    Don't load lowering images on mobile
    Only render the best-fitting resolution on mobile devices (determined by screen size) by enabling immediateRender in OpenSeadragon.

--- src/views/Scan.vue
+++ src/views/Scan.vue
@@ -93,6 +93,7 @@
 				this.viewer = openSeadragon({
 					animationTime: .5,
 					id: 'scan_image',
+					immediateRender: this.$root.isMobile(),
 					preserveImageSizeOnResize: true,
 					preserveViewport: true,
 					showNavigationControl: false,
