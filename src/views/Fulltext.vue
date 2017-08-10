296937218368adfca8cf01e81de6ecca915fd4af
tschaef <tschaef1@gwdg.de>
2017-08-10 13:45:05 +0200

    Extend fulltext support
    Display not only remotely loaded fulltexts but also those provided via the `char` property.

--- src/views/Fulltext.vue
+++ src/views/Fulltext.vue
@@ -2,14 +2,16 @@
 	<section class="tify-fulltext">
 		<h2 class="tify-sr-only">{{ 'Fulltext'|trans }}</h2>
 
-		<div v-if="fulltexts.length" class="tify-fulltext_texts">
-			<template v-for="text, index in fulltexts">
-				<hr v-if="index > 0" class="tify-fulltext_separator">
-				<div v-html="text" class="tify-fulltext_text"/>
+		<div v-if="fulltextAvailable" class="tify-fulltext_texts">
+			<template v-for="page, index in this.$root.params.pages" v-if="page">
+				<hr v-if="index && page > 1" class="tify-fulltext_separator">
+				<template v-for="text in fulltexts[page]">
+					 <div class="tify-fulltext_text" v-html="text"/>
+				</template>
 			</template>
 		</div>
 		<div v-else class="tify-fulltext_none">
-			{{ 'No fulltext available for this page'|trans }}
+			{{ 'Fulltext not available for this page'|trans }}
 		</div>
 	</section>
 </template>
@@ -18,40 +20,43 @@
 	export default {
 		data() {
 			return {
+				fulltextAvailable: false,
 				fulltexts: [],
-				loadedFulltexts: {},
 			};
 		},
 		watch: {
 			// eslint-disable-next-line func-names
 			'$root.params.pages': function () {
-				this.loadFulltext();
+				this.loadFulltexts();
 			},
 		},
 		methods: {
-			loadFulltext() {
+			loadFulltexts() {
+				this.fulltextAvailable = false;
 				this.fulltexts = [];
-				this.$root.params.pages.forEach((page) => {
-					if (page < 1) return;
 
-					if (this.loadedFulltexts[page]) {
-						this.fulltexts.push(this.loadedFulltexts[page]);
-						return;
-					}
+				this.$root.params.pages.forEach((page) => {
+					if (page < 1 || this.fulltexts[page]) return;
 
 					const canvas = this.$root.canvases[page - 1];
 					if (!('otherContent' in canvas)) return;
 
+					this.$set(this.fulltexts, page, []);
+
 					const annotationListUrl = canvas.otherContent[0]['@id'];
 					this.$http.get(annotationListUrl).then((response) => {
-						const fulltextUrl = response.data.resources[0].resource['@id'];
-						this.$http.get(fulltextUrl).then((response2) => {
-							const fulltext = this.$options.filters.filterHtml(response2.data);
-							this.fulltexts.push(fulltext);
-							this.loadedFulltexts[page] = fulltext;
-						}, (error) => {
-							const status = (error.response ? error.response.statusText : error.message);
-							this.$root.error = `Error loading fulltext: ${status}`;
+						const resources = response.data.resources;
+						if (!Array.isArray(resources)) return;
+
+						resources.forEach((resource, index) => {
+							const res = resource.resource;
+							if (res && res.chars) {
+								const text = this.$options.filters.filterHtml(res.chars);
+								if (text) this.fulltextAvailable = true;
+								this.$set(this.fulltexts[page], index, text);
+							} else if (res['@id']) {
+								this.loadRemoteFulltext(page, index, res['@id']);
+							}
 						});
 					}, (error) => {
 						const status = (error.response ? error.response.statusText : error.message);
@@ -59,9 +64,19 @@
 					});
 				});
 			},
+			loadRemoteFulltext(page, index, url) {
+				this.$http.get(url).then((response2) => {
+					const text = this.$options.filters.filterHtml(response2.data);
+					if (text) this.fulltextAvailable = true;
+					this.$set(this.fulltexts[page], index, text);
+				}, (error) => {
+					const status = (error.response ? error.response.statusText : error.message);
+					this.$root.error = `Error loading fulltext: ${status}`;
+				});
+			},
 		},
 		mounted() {
-			this.loadFulltext();
+			this.loadFulltexts();
 		},
 	};
 </script>
