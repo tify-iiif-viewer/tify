884d1887131219694975f0ece56faf39860298f9
tschaef <tschaef1@gwdg.de>
2017-09-21 16:51:10 +0200

    Update label in key binding help
    Use "Contents" instead of "Table of Contents" to match the corresponding button label.

--- src/views/Help.vue
+++ src/views/Help.vue
@@ -24,7 +24,7 @@
 				</tr>
 				<tr>
 					<th>
-						{{ 'Table of Contents'|trans }}
+						{{ 'Contents'|trans }}
 						<small v-html="$options.filters.trans('(if&nbsp;available)')"/>
 					</th>
 					<td>
