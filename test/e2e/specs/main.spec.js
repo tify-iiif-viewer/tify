4649997ca850b6fa9693a7cf990af60daeee2e36
tschaef <tschaef1@gwdg.de>
2017-11-15 16:07:02 +0100

    Test for missing translations
    And complete German translation.

--- test/e2e/specs/main.spec.js
+++ test/e2e/specs/main.spec.js
@@ -1,10 +1,10 @@
 Feature('Main');
 
 Scenario('Start the app', (I) => {
-	I.amOnPage('http://localhost:8080/?manifest=http://localhost:8081/manifest/wellcome-b18035723.json');
+	I.amOnPage('http://localhost:8080/?manifest=http://localhost:8081/manifest/gdz-PPN857449303.json&language=de');
 	I.waitForElement('.tify-app_main');
 
 	I.seeElement('#tify > .tify-app');
 
-	I.see('Wunder der Vererbung', 'h1');
+	I.dontSeeInSource('[missing translation]');
 });
