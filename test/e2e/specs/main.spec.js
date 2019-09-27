36d65d306986ba590e6ee531afd2e63742628613
Ingo Pfennigstorf <i.pfennigstorf@gmail.com>
2019-09-27 15:20:17 +0200

    Split fullscreen tests
    This splits the fullscreen tests into one test via mouse and one
    via keyboard.
    Also leaving the fullscreen mode is tested via the Escape key.

--- test/e2e/specs/main.spec.js
+++ test/e2e/specs/main.spec.js
@@ -7,10 +7,10 @@ Scenario('Start the app', (I) => {
 	I.seeElement('#tify > .tify-app');
 });
 
-Scenario('Toggle fullscreen', (I) => {
+Scenario('Toggle fullscreen via keyboard', (I) => {
 	I.amOnPage('http://localhost:8080/?manifest=http://localhost:8081/manifest/gdz-HANS_DE_7_w042081.json');
+	I.resizeWindow(1600, 800);
 	I.waitForElement('.tify-app_main');
-
 	I.see('Fullscreen', 'button');
 	I.dontSee('Exit fullscreen', 'button');
 
@@ -19,8 +19,26 @@ Scenario('Toggle fullscreen', (I) => {
 	I.pressKey('u');
 	I.see('Fullscreen', 'button');
 
+	I.pressKey('u');
+	I.see('Exit fullscreen', 'button');
+	I.pressKey('Esc');
+	I.see('Fullscreen', 'button');
+});
+
+Scenario('Toggle fullscreen via mouse', (I) => {
+	I.amOnPage('http://localhost:8080/?manifest=http://localhost:8081/manifest/gdz-HANS_DE_7_w042081.json');
+	I.resizeWindow(1600, 800);
+	I.waitForElement('.tify-app_main');
+	I.see('Fullscreen', 'button');
+	I.dontSee('Exit fullscreen', 'button');
+
 	I.click('Fullscreen');
 	I.see('Exit fullscreen', 'button');
 	I.click('Exit fullscreen');
+	I.see('Fullscreen', 'button');
+
+	I.click('Fullscreen');
+	I.see('Exit fullscreen', 'button');
+	I.pressKey('Esc');
 	I.see('Fullscreen', 'button');
 });
