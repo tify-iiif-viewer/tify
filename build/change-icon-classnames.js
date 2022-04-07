36fcbbfd5e8074dc778af28aa3ccb922680f1670
t11r <1674104+t11r@users.noreply.github.com>
2022-04-07 14:49:15 +0200

    Add workaround for build script
    `build/change-icon-classnames.js` is also run when TIFY is installed via `npm install tify`, which is when `node_modules` are not available.

--- build/change-icon-classnames.js
+++ build/change-icon-classnames.js
@@ -1,8 +1,12 @@
-const replace = require('replace-in-file');
+try {
+	const replace = require('replace-in-file');
 
-// Replace class names in icon files
-replace.sync({
-	files: `${__dirname}/../node_modules/vue-material-design-icons/*.vue`,
-	from: [/class="material-design-icon .*?"/g, /\s*class="material-design-icon__svg"/],
-	to: ['class="tify-icon"', ''],
-});
+	// Replace class names in icon files
+	replace.sync({
+		files: `${__dirname}/../node_modules/vue-material-design-icons/*.vue`,
+		from: [/class="material-design-icon .*?"/g, /\s*class="material-design-icon__svg"/],
+		to: ['class="tify-icon"', ''],
+	});
+} catch (error) {
+	// Nothing to do here
+}
