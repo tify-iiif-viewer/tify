43cbe9d2d8f39462935f832067a4440f448016bd
t11r <1674104+t11r@users.noreply.github.com>
2022-10-16 17:53:09 +0200

    Make URLs in metadata clickable
    Change info panel and introduce a proper URL validation function.

---
+++ src/functions/isValidUrl.js
@@ -0,0 +1,11 @@
+export default function isValidUrl(string, allowedProtocols = ['https:', 'http:']) {
+	let url;
+
+	try {
+		url = new URL(string);
+	} catch {
+		return false;
+	}
+
+	return allowedProtocols.includes(url.protocol);
+}
