d36775dc1cc1311b90990b55855b932dc87a822a
t11r <1674104+t11r@users.noreply.github.com>
2024-03-25 16:12:36 +0100

    Add e2e tests for native IIIF 3 manifests

---
+++ tests/e2e/iiif3.spec.js
@@ -0,0 +1,47 @@
+describe('Support for native IIIF 3 manifests', () => {
+	it('displays correct metadata', () => {
+		cy.visit(
+			`/?manifest=${Cypress.env('iiifApiUrl')}/manifest/aku-pal-375`
+			+ '&tify={"view":"info"}',
+		);
+
+		cy.get('.tify').then(() => {
+			cy.contains('h1', 'Dipinto Assiut, Gebel Assiut al-gharbi, N13.1 TN2');
+			cy.contains('a', 'AKU-PAL database record');
+		});
+	});
+
+	it('displays the TOC', () => {
+		cy.visit(
+			`/?manifest=${Cypress.env('iiifApiUrl')}/manifest/amherst-63cc6105-570d-407b-af8c-07fda3f8c620.json`
+			+ '&tify={"view":"toc"}',
+		);
+
+		cy.get('.tify').then(() => {
+			cy.contains('.tify-toc-link', 'Transcription of Emily Dickinson\'s "I\'ll clutch - and clutch" - Image 3');
+			cy.get('.tify-toc-link').should('have.length', 1);
+		});
+	});
+
+	it('displays attribution HTML', () => {
+		cy.visit(
+			`/?manifest=${Cypress.env('iiifApiUrl')}/manifest/mskgent-8210.json`
+			+ '&tify={"view":"info"}',
+		);
+
+		cy.get('.tify').then(() => {
+			cy.get('.tify-info-section.-attribution').find('img'); // logo in attribution section
+		});
+	});
+
+	it('displays a dash for metadata with an empty value', () => {
+		cy.visit(
+			`/?manifest=${Cypress.env('iiifApiUrl')}/manifest/utrecht-1874-325480`
+			+ '&tify={"view":"info"}',
+		);
+
+		cy.get('.tify-info-metadata').then(() => {
+			cy.contains('h4', 'Published').next().should('have.text', '—'); // &mdash;
+		});
+	});
+});
