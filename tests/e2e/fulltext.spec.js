5247da1f9f36afc05cf07cf75c0fd12e5a959c0e
t11r <1674104+t11r@users.noreply.github.com>
2024-06-10 16:05:30 +0200

    Simplify end-to-end tests
    Remove unnecessary checks for wrapper element.

--- tests/e2e/fulltext.spec.js
+++ tests/e2e/fulltext.spec.js
@@ -1,16 +1,14 @@
 describe('Fulltext', () => {
 	it('displays fulltext', () => {
 		cy.visit(`/?manifest=${Cypress.env('iiifApiUrl')}/manifest/wellcome-b18035723&tify={"pages":[15]}`);
-		cy.get('.tify').then(() => {
-			cy.contains('Fulltext').click();
-			cy.contains('Alles höhere Leben - ob Tier oder').should('be.visible');
-		});
+
+		cy.contains('Fulltext').click();
+		cy.contains('Alles höhere Leben - ob Tier oder').should('be.visible');
 	});
 
 	it('displays XML fulltext', () => {
 		cy.visit(`/?manifest=${Cypress.env('iiifApiUrl')}/manifest/gdz-PPN235181684_0029&tify={"view":"fulltext"}`);
-		cy.get('.tify').then(() => {
-			cy.contains('Unter Mitwirkung der Herren').should('be.visible');
-		});
+
+		cy.contains('Unter Mitwirkung der Herren').should('be.visible');
 	});
 });
