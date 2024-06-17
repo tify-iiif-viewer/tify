f6213ea7ccad6aba995220af26b71b74d92bb582
t11r <1674104+t11r@users.noreply.github.com>
2024-06-17 17:18:47 +0200

    Gracefully handle a missing translation
    If a translation file cannot be loaded, display an error message and keep the previous language. Allow setting the language via URL query for testing.
    
    Resolves #170

--- tests/e2e/main.spec.js
+++ tests/e2e/main.spec.js
@@ -19,4 +19,24 @@ describe('Main', () => {
 		cy.visit(`/?manifest=${Cypress.env('iiifApiUrl')}/manifest/not-json`);
 		cy.contains('Error loading IIIF manifest');
 	});
+
+	it('loads a translation', () => {
+		cy.visit(`/?manifest=${Cypress.env('iiifApiUrl')}/manifest/gdz-PPN857449303&language=de`);
+
+		cy.get('.tify-header');
+		cy.contains('Seiten');
+		cy.contains('Inhalt');
+	});
+
+	it('gracefully handles a missing translation', () => {
+		// Prevent the test from failing due to an uncaught exception (which is expected)
+		cy.on('uncaught:exception', () => false);
+
+		cy.visit(`/?manifest=${Cypress.env('iiifApiUrl')}/manifest/gdz-PPN857449303&language=nope`);
+
+		cy.get('.tify-header');
+		cy.contains('Pages');
+		cy.contains('Contents');
+		cy.contains('Error loading translation for "nope"');
+	});
 });
