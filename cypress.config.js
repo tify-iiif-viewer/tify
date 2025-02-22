48dae8da8959dd574c37da82e28de3e0f86434d4
t11r <1674104+t11r@users.noreply.github.com>
2025-02-22 21:23:32 +0100

    Validate HTML after each end-to-end test step
    Fix issues in index.html and templates to pass validation.

--- cypress.config.js
+++ cypress.config.js
@@ -1,5 +1,8 @@
 import { defineConfig } from 'cypress';
 
+// eslint-disable-next-line import/no-extraneous-dependencies
+import htmlvalidate from 'cypress-html-validate/plugin';
+
 // eslint-disable-next-line import/extensions
 import server from './tests/iiif-api/server.js';
 
@@ -19,7 +22,11 @@ export default defineConfig({
 		screenshotsFolder: 'tests/e2e/screenshots',
 		videosFolder: 'tests/e2e/videos',
 		downloadsFolder: 'tests/e2e/downloads',
-		supportFile: false,
+		supportFile: 'tests/e2e/support/e2e.js',
+
+		setupNodeEvents(on) {
+			htmlvalidate.install(on);
+		},
 	},
 	env: {
 		iiifApiUrl: `http://0.0.0.0:${iiifApiPort}`,
---
+++ tests/e2e/support/e2e.js
@@ -0,0 +1,29 @@
+// eslint-disable-next-line import/no-extraneous-dependencies
+import 'cypress-html-validate/commands';
+
+afterEach(() => {
+	if (cy.bypassAfterEach) {
+		return;
+	}
+
+	cy.htmlvalidate(
+		{
+			rules: {
+				'heading-level': [
+					'error',
+					{
+						allowMultipleH1: true,
+					},
+				],
+				'prefer-native-element': 'off',
+				'require-sri': 'off',
+			},
+		},
+		{
+			exclude: [
+				// Attribution may contain invalid HTML if the manifest provides such.
+				'.tify-info-section.-attribution',
+			],
+		},
+	);
+});
