83eb78e07158e9d80466171ffd8f268e5db18bc4
t11r <1674104+t11r@users.noreply.github.com>
2025-07-03 02:35:57 +0200

    Add Stylelint, fix SCSS linting issues

---
+++ stylelint.config.js
@@ -0,0 +1,33 @@
+/** @type {import('stylelint').Config} */
+export default {
+	extends: [
+		// NOTE: Order of extends matters
+		'@stylistic/stylelint-config',
+		'stylelint-config-standard-scss',
+	],
+	plugins: [
+		'stylelint-order',
+	],
+	rules: {
+		'declaration-empty-line-before': [
+			'never',
+			{ ignore: ['after-declaration'] },
+		],
+		'order/order': [
+			'custom-properties',
+			'declarations',
+		],
+		'order/properties-alphabetical-order': true,
+		'selector-attribute-quotes': 'never',
+		'selector-class-pattern': [
+			'^(-?[a-z][a-z0-9-]*[a-z0-9])$',
+		],
+		'@stylistic/block-closing-brace-newline-after': [
+			'always',
+			{ ignoreAtRules: ['if', 'else'] },
+		],
+		'@stylistic/indentation': 'tab',
+		'@stylistic/number-leading-zero': 'never',
+		'@stylistic/string-quotes': 'single',
+	},
+};
