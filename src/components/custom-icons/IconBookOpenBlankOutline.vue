a36cfc7ef83aeccdb0766f927bc4076badb8e9b5
t11r <1674104+t11r@users.noreply.github.com>
2025-08-14 19:30:31 +0200

    Replace icon for double-page button

---
+++ src/components/custom-icons/IconBookOpenBlankOutline.vue
@@ -0,0 +1,11 @@
+<template>
+	<svg
+		class="tify-icon"
+		aria-hidden="true"
+		viewBox="0 0 24 24"
+	>
+		<path
+			d="M21,4H3A2,2 0 0,0 1,6V19A2,2 0 0,0 3,21H21A2,2 0 0,0 23,19V6A2,2 0 0,0 21,4M3,19V6H11V19H3M21,19H13V6H21V19Z"
+		/>
+	</svg>
+</template>
