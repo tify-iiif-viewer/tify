6cd57bde54324887001ef5088016328f3d778b4a
t11r <1674104+t11r@users.noreply.github.com>
2025-08-25 19:59:01 +0200

    Improve thumbnails
    - Change HTML for better a11y
    - Display icon for missing thumbnails
    - Update styles

--- tests/e2e/thumbnails.spec.js
+++ tests/e2e/thumbnails.spec.js
@@ -1,9 +1,11 @@
-it('handles a missing thumbnail', () => {
-	cy.visit(
-		`/?manifest=${Cypress.env('iiifApiUrl')}/manifest/cookbook-recipe-0283-missing-image`
-		+ '&tify={"view":"thumbnails"}',
-	);
+describe('Thumbnails', () => {
+	it('handles a missing thumbnail', () => {
+		cy.visit(
+			`/?manifest=${Cypress.env('iiifApiUrl')}/manifest/cookbook-recipe-0283-missing-image`
+			+ '&tify={"view":"thumbnails"}',
+		);
 
-	cy.get('.tify-thumbnails-item').should('have.length', 4);
-	cy.get('.tify-thumbnails-item:eq(1) img[src="data:,"]');
+		cy.get('.tify-thumbnails-item').should('have.length', 4);
+		cy.contains('.tify-thumbnails-item:eq(1)', 'Image missing');
+	});
 });
